934d6a35b21034b4a52a203d3356558a173ca293
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-27 23:06:25 -0300

    Add toast provider on app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import { ToastContainer } from 'react-toastify';
 
 import '../styles/global.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <Header />
+      <ToastContainer />
       <Component {...pageProps} />
     </NextAuthProvider>
   );
